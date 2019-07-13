15aa9484858a306916884e76ca591fc024f58a28
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-07-13 21:11:20 +0300

    Save and Delete Content in Express with HTTP Verbs

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var express = require("express");
 var fs = require("fs");
+var path = require("path");
 var _ = require("lodash");
 var engines = require("consolidate");
+var bodyParser = require("body-parser");
 
 var app = express();
-var users = [];
 
-fs.readFile("users.json", { encoding: "utf8" }, function(err, data) {
-  if (err) throw err;
+function getUserFilePath(username) {
+  return path.join(__dirname, "users", username) + ".json";
+}
 
-  JSON.parse(data).forEach(function(user) {
-    user.name.full = _.startCase(user.name.first + " " + user.name.last);
-    users.push(user);
+function getUser(username) {
+  var user = JSON.parse(
+    fs.readFileSync(getUserFilePath(username), { encoding: "utf8" })
+  );
+
+  user.name.full = _.startCase(user.name.first + " " + user.name.last);
+  _.keys(user.location).forEach(function(key) {
+    user.location[key] = _.startCase(user.location[key]);
   });
-});
+  return user;
+}
+
+function saveUser(username, data) {
+  var fp = getUserFilePath(username);
+
+  fs.unlinkSync(fp); // delete the file
+  fs.writeFileSync(fp, JSON.stringify(data, null, 2), { encoding: "utf8" });
+}
 
 app.engine("hbs", engines.handlebars);
 
@@ -21,26 +36,60 @@ app.set("views", "./views");
 app.set("view engine", "hbs");
 
 app.use("/profilepics", express.static("images"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/favicon.ico", function(req, res) {
+  res.end();
+});
 
 app.get("/", function(req, res) {
-  res.render("index", { users: users });
-});
+  var users = [];
 
-app.get(/big.*/, function(req, res, next) {
-  console.log("BIG USER ACCESS");
-  next();
-});
+  fs.readdir("users", function(err, files) {
+    files.forEach(function(file) {
+      fs.readFile(
+        path.join(__dirname, "users", file),
+        { encoding: "utf8" },
+        function(err, data) {
+          var user = JSON.parse(data);
 
-app.get(/.*dog.*/, function(req, res, next) {
-  console.log("DOGS GO WOOF");
-  next();
+          user.name.full = _.startCase(user.name.first + " " + user.name.last);
+          users.push(user);
+
+          if (users.length === files.length)
+            res.render("index", { users: users });
+        }
+      );
+    });
+  });
 });
 
 app.get("/:username", function(req, res) {
   var username = req.params.username;
-  res.send(username);
+  var user = getUser(username);
+
+  res.render("user", {
+    user: user,
+    address: user.location
+  });
+});
+
+app.put("/:username", function(req, res) {
+  var username = req.params.username;
+  var user = getUser(username);
+
+  user.location = req.body;
+  saveUser(username, user);
+  res.end();
+});
+
+app.delete("/:username", function(req, res) {
+  var fp = getUserFilePath(req.params.username);
+
+  fs.unlinkSync(fp); // delete the file
+  res.sendStatus(200);
 });
 
 var server = app.listen(3000, function() {
-  console.log("Server running at 'http://localhost:" + server.address().port);
+  console.log("Server running at http://localhost:" + server.address().port);
 });
